63e8574d12dc2d3404dfacc04131a526afef6a42
Jameson Farmer <thejamesonfarmer@gmail.com>
2026-01-23 12:02:04 -0800

    added button to render ultrasound form and ultrasound component

--- frontend/src/components/makeReservation.jsx
+++ frontend/src/components/makeReservation.jsx
@@ -1,7 +1,15 @@
+import React, { useState } from 'react';
+import UltraSound from "./UltraSound";
 function MakeReservation() {
+    const [isVisible, setIsVisible] = useState(false);
+    const handleClick = () => {
+        setIsVisible(true);
+    }
     return(
         <>
             <p>Make a reservation</p>
+            <button onClick={handleClick}>Ultrasound</button>
+            {isVisible && <UltraSound />}
         </>
     )
 }
