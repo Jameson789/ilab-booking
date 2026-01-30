1751fcc4cb68f07b9c8867e9a6077f66f42c8d8a
Augy Markham <katiefaithmarkham@gmail.com>
2026-01-29 20:22:43 -0800

    preserve form data in state

--- next-frontend/components/form/ReserveForm.jsx
+++ next-frontend/components/form/ReserveForm.jsx
@@ -1,31 +1,63 @@
 'use client'
+import { useState } from 'react'
 import '../../app/globals.css'
 function ReserveForm() {
+    const [formData, setFormData] = useState({
+        piName: '',
+        studyName: '',
+        studyFocus: '',
+        billingCode: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
     return (
         <>
             <h2>*Study* Form </h2>
-                <div className="form-section">
-                    <form>
-                        <label>PI Name</label>
-                        <br/>
-                        <input></input>
-                        <br/>
+            <div className="form-section">
+                <form>
+                    <label>PI Name</label>
+                    <br/>
+                    <input 
+                        name="piName"
+                        value={formData.piName}
+                        onChange={handleChange}
+                    />
+                    <br/>
 
-                        <label>Study Name</label>
-                        <br/>
-                        <input></input>
-                        <br/>
+                    <label>Study Name</label>
+                    <br/>
+                    <input 
+                        name="studyName"
+                        value={formData.studyName}
+                        onChange={handleChange}
+                    />
+                    <br/>
 
-                        <label>Study Focus</label>
-                        <br/>
-                        <textarea></textarea>
-                        <br/>
+                    <label>Study Focus</label>
+                    <br/>
+                    <textarea 
+                        name="studyFocus"
+                        value={formData.studyFocus}
+                        onChange={handleChange}
+                    />
+                    <br/>
 
-                        <label>Billing Code</label>
-                        <br/>
-                        <input></input>
-                    </form>
-                </div>
+                    <label>Billing Code</label>
+                    <br/>
+                    <input 
+                        name="billingCode"
+                        value={formData.billingCode}
+                        onChange={handleChange}
+                    />
+                </form>
+            </div>
         </>
     )
 }
