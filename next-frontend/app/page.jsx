7df5a9377191eeeb7afdce1800b6384cc77b8809
rifflere <82406585+rifflere@users.noreply.github.com>
2026-02-03 10:58:19 -0800

    Removed unused component

--- next-frontend/app/page.jsx
+++ next-frontend/app/page.jsx
@@ -2,7 +2,6 @@
 
 import MakeReservationSection from "@/components/main-page/MakeReservationSection";
 import ReservationsSection from "@/components/main-page/ReservationsSection";
-import UltraSound from "@/components/main-page/UltraSound";
 import "./globals.css";
 
 export default function HomePage() {
@@ -11,8 +10,6 @@ export default function HomePage() {
       <h1>Fred Hutch Booking</h1>
       <MakeReservationSection />
       <ReservationsSection />
-      
-      <UltraSound />
     </main>
   );
 }
