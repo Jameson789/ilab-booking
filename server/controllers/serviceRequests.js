8a3633a15c2fa3a53f58c57b93ceaa720af141cf
rifflere <82406585+rifflere@users.noreply.github.com>
2026-02-23 10:36:03 -0800

    Configured iLab API service

--- server/controllers/serviceRequests.js
+++ server/controllers/serviceRequests.js
@@ -1,3 +1,11 @@
+import { fetchServiceRequests } from "../services/ilabApiService.js";
+
 export const getAllValidServiceRequests = async (req, res) => {
-    res.status(200).json({ message: "Success" })
+    try {
+        const data = await fetchServiceRequests();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching service requests: ', error.message);
+        res.status(500).json({ error: 'Failed to fetch service requests' });
+    } 
 };
---
+++ server/services/ilabApiService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { ilabConfig } from '../config/ilabConfig.js';
+
+const ilabClient = axios.create({
+    baseURL: ilabConfig.baseURL,
+    headers: {
+        Authorization: `Bearer ${ilabConfig.token}`,
+        'Content-Type': 'application/json',
+    },
+})
+
+export const fetchServiceRequests = async () => {
+    const res = await ilabClient.get('/v1/cores/4665/service_requests');
+    return res.data;
+}
