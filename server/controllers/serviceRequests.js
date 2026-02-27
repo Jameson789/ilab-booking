aeab52dda822be6a475d621f7b940a6b9c88e403
rifflere <82406585+rifflere@users.noreply.github.com>
2026-02-27 14:03:59 -0800

    Added create service request logic with basic validation to server

--- server/controllers/serviceRequests.js
+++ server/controllers/serviceRequests.js
@@ -1,4 +1,4 @@
-import { fetchServiceRequests } from "../services/ilabApiService.js";
+import { fetchServiceRequests, postServiceRequests } from "../services/ilabApiService.js";
 
 export const getAllValidServiceRequests = async (req, res) => {
     try {
@@ -10,3 +10,29 @@ export const getAllValidServiceRequests = async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch service requests' });
     } 
 };
+
+export const createServiceRequest = async (req, res) => {
+    const { owner_email, pi_email, name, state } = req.body.service_request;
+    
+    if (!owner_email) {
+        return res.status(400).json({error: 'Missing owner email'})
+    }
+        if (!pi_email) {
+        return res.status(400).json({error: 'Missing pi email'})
+    }
+    if (!name) {
+        return res.status(400).json({error: 'Missing study name'})
+    }
+    if (!state) {
+        return res.status(400).json({error: 'Missing state'})
+    }
+
+    try {
+        const payload = {}
+        const data = {}
+        res.status(201).json(data);
+    } catch (err) {
+        console.error('ERROR: ', err.message);
+        res.status(500).json({ error: '' })
+    }
+}
