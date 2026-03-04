0dd772137c41d2579b9d8a92f3d7b320c7f85bdb
Augy Markham <katiefaithmarkham@gmail.com>
2026-03-04 11:37:52 -0800

    Updated the Available Services section with styling

--- next-frontend/app/theme/theme.js
+++ next-frontend/app/theme/theme.js
@@ -3,8 +3,8 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: { main: "#18365D" },     // Dark Navy
-    secondary: { main: "#AA4AC4" },   // Warm Purple
-    info: { main: "#00ABC8" },        // Bright Blue
+    secondary: { main: "#00ABC8" },   // Warm Purple
+    info: { main:  "#AA4AC4"},        // Bright Blue
     warning: { main: "#FFB500" },     // Vivid Yellow
     background: {
       default: "#F4F4F4",             // Light Grey
---
+++ next-frontend/components/main-page/AvailableServices.jsx
@@ -0,0 +1,41 @@
+/*
+  This component render the header and all active equipment card components.
+*/
+
+"use client";
+
+import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+import ReservationButton from "./EquipmentCard";
+
+function AvailableServices() {
+
+  return (
+    <>
+      <Typography 
+        variant="h4" 
+        component="h2"
+        color="secondary" 
+        gutterBottom>
+        Available Services
+      </Typography>
+      <Grid container spacing = {2}>
+        <Grid size={4}>
+          <ReservationButton machineName="MRI" />
+        </Grid>
+        <Grid size={4}>
+          <ReservationButton machineName="Ultrasound" />
+        </Grid>
+        <Grid size={4}>
+          <ReservationButton machineName="Other"/>
+        </Grid>
+      </Grid>
+      
+      
+    </>
+  );
+}
+
+export default AvailableServices;
